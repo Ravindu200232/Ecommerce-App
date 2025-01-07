0527c70aba649f61ffd1cb1bb0a6b8fd0b167a83
Ravindu200232 <it22098450@my.sliit.lk>
2025-01-07 23:20:02 +0530

    create login.js in frontend 😘😘

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 import Logo from './Logo'
 import { GrSearch } from "react-icons/gr";
 import { FaRegCircleUser } from "react-icons/fa6";
@@ -7,10 +7,13 @@ import { FaShoppingCart } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <header className='h-16 shadow-md'>
+    <header className='h-16 shadow-md bg-white'>
         <div className='h-full container mx-auto flex items-center px-4 justify-between'>
             <div className=''>
-                <Logo w={90} h={50}></Logo>
+              <Link to={"/"}>
+              <Logo w={90} h={50}></Logo>
+              </Link>
+                
             </div>
             <div className=' hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow pl-2'>
                 <input type='text' placeholder='search product here....' className='w-full outline-none'/>
@@ -33,7 +36,7 @@ const Header = () => {
               </div>
 
                <div>
-                <button className='px-3 py-1 rounded-full text-white bg-red-600 hover:bg-red-700'>Login</button>
+                <Link to={"/login"} className='px-3 py-1 rounded-full text-white bg-red-600 hover:bg-red-700'>Login</Link>
                </div>
 
             </div>
